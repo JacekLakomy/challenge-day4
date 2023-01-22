017e63153928bbcbe79e13a4518c5a0b520a6083
Jacek Łakomy <jac.lakomy@gmail.com>
2023-01-22 12:50:05 +0100

    dzień 4 - gotowe zadania

---
+++ challenge21-day4/challenge21-day4/Program.cs
@@ -0,0 +1,19 @@
+﻿var name = "Ewa";
+char sex = 'F';
+var age = 25;
+
+if (sex == 'F')
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+}
+else if (age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
